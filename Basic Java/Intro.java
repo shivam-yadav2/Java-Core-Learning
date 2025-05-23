ddf28336ed782cae9e07d39fd4f724f37b7dcd36
shivam-yadav2 <shivsupertech@gmail.com>
2025-05-23 14:03:31 +0530

    variables

--- Basic Java/Intro.java
+++ Basic Java/Intro.java
@@ -2,6 +2,11 @@
 class intro {
 
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        int num = 3;
+        int num2 = 3;
+        int sum  = num + num2;
+
+        System.out.println(sum);
+        System.out.println(13+5);
     }
 }
