cee3a6f3a90892173320bee6aa4e9f036b154e3e
shivam-yadav2 <shivsupertech@gmail.com>
2025-05-24 17:28:25 +0530

    data types and literals

--- Basic Java/Intro.java
+++ Basic Java/Intro.java
@@ -2,11 +2,21 @@
 class intro {
 
     public static void main(String[] args) {
-        int num = 3;
-        int num2 = 3;
-        int sum  = num + num2;
+        // int num = 3;
+        // int num2 = 3;
+        // int sum  = num + num2;
 
-        System.out.println(sum);
-        System.out.println(13+5);
+        // System.out.println(sum);
+        // System.out.println(13+5);
+
+        int num = 3;
+        byte by = 127;
+        short sh = 32767;
+        long lo = 123456789l;
+
+        float fl = 3.14f;
+        double db = 3.14;
+        char ch = 'a';
+        boolean bl = true;
     }
 }
