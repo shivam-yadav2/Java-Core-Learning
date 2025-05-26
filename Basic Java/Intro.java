cc015c5e7b099213017dc4a02e747c68ea722523
shivam-yadav2 <shivsupertech@gmail.com>
2025-05-26 12:31:05 +0530

    type conversions

--- Basic Java/Intro.java
+++ Basic Java/Intro.java
@@ -9,14 +9,32 @@ class intro {
         // System.out.println(sum);
         // System.out.println(13+5);
 
-        int num = 3;
-        byte by = 127;
-        short sh = 32767;
-        long lo = 123456789l;
+        // int num = 3;
+        // byte by = 127;
+        // short sh = 32767;
+        // long lo = 123456789l;
 
-        float fl = 3.14f;
-        double db = 3.14;
-        char ch = 'a';
-        boolean bl = true;
+        // float fl = 3.14f;
+        // double db = 3.14;
+        // char ch = 'a';
+        // boolean bl = true;
+
+
+        // ==================== Type Conversion ======================== 
+
+        // int x = 257;
+        // // System.out.println(x);
+
+        // byte a = (byte)x ;
+        // System.out.println(a);
+
+        // float f = 5.7f;
+        // int i = (int)f;
+        // System.out.println(i);
+
+
+        // ========================= Operators ======================== 
+        
+
     }
 }
