7b8d5a69a12d94df473e45a55e413effc8acfe5c
shivam-yadav2 <shivsupertech@gmail.com>
2025-06-24 14:01:59 +0530

    add and swap

--- Basic Java/Ops.java
+++ Basic Java/Ops.java
@@ -1,13 +1,32 @@
 class Calculate {
     public int add (int a , int b){
+        int res = a + b;
+        System.out.println("The sum is Add fun: " + res);
+        // System.out.println("The sum is from dd: " + a+b);
         return a+b;
     }
 }
 
-class Ops {
+
+class swap extends Calculate {
+    public void swapNum(int a , int b){
+        a= a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+
+        add(5, 10); // Calling the add method from Calculate class
+    }
+}
+
+class Ops  {
     public static void main(String[] args) {
         Calculate calc = new Calculate();
-        int result = calc.add(5, 10);
+        swap sw = new swap();
+        int result = sw.add(5, 10);
         System.out.println("The sum is: " + result);
+        System.out.println("Before swapping: a = 5, b = 3");
+
+        sw.swapNum(5,3);
     }
 }
